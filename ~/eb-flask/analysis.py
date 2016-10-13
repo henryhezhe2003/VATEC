e71df0e9b7c0831a65a409ab0ca3fcdbe8a65e0c
Zhiwei Chen <z.w.chan.jason@gmail.com>
2016-10-13 12:00:58 -0400

    display inclusion and exclusion

--- ~/eb-flask/analysis.py
+++ ~/eb-flask/analysis.py
@@ -26,24 +26,24 @@ def _analysis(mysql):
     cur = db.cursor()
 
     #sql = "SELECT distinct V.TID, V.month FROM cancer_cui V, meta T where %s" %(curr_condition) + " and T.tid = V.tid and "+ phase_query + " and "+ status_query + " and "+ study_type_query + " and "+ intervention_type_query + " and "+ agency_type_query + " and "+ gender_query + " and "+ start_date_query + " and "+ age_query + " and "+ intervention_model_query + " and "+ allocation_query + " and "+ time_perspective_query + " and "+ disease_query
-    sql = "SELECT V.month, count(*), count(distinct V.tid) FROM cancer_cui V, meta T where %s and %s group by month order by month"  % (curr_condition, filter_builder)
+    sql = "SELECT V.month,SUM(V.type='INCLUSION'), SUM(V.type='EXCLUSION') FROM cancer_cui V, meta T where %s and %s group by V.month order by month"  % (curr_condition, filter_builder)
     print sql
     cur.execute(sql)
     modal_boundary_output_simple = []
-    modal_boundary_output_simple.append(["value", "Frequency"])
+    modal_boundary_output_simple.append(["value", "INCLUSION", "EXCLUSION"])
     num_of_trials = 0
     for row in cur.fetchall():
-        modal_boundary_output_simple.append([str(row[0]),int(row[1])])
+        modal_boundary_output_simple.append([str(row[0]),int(row[1]),int(row[2])])
         num_of_trials += int(row[2])
 
     # pattern distribution
-    sql = "SELECT V.pattern, count(*) as freq FROM cancer_cui V, meta T where %s and %s group by pattern order by freq desc"  % (curr_condition, filter_builder)
+    sql = "SELECT V.pattern, SUM(V.type='INCLUSION'), SUM(V.type='EXCLUSION') as freq FROM cancer_cui V, meta T where %s and %s group by pattern order by freq desc"  % (curr_condition, filter_builder)
     print sql
     cur.execute(sql)
     pattern_freq_output_simple = []
-    pattern_freq_output_simple.append(["pattern", "Frequency"])
+    pattern_freq_output_simple.append(["pattern", "INCLUSION","EXCLUSION"])
     for row in cur.fetchall():
-        pattern_freq_output_simple.append([str(row[0]),int(row[1])])
+        pattern_freq_output_simple.append([str(row[0]),int(row[1]),int(row[2])])
 
 
     if (len(modal_boundary_output_simple) > 0):
